8e0a36ab7e7ac4ef9806bc0dac53f4684094036c
FeedTheMind <FeedTheMind@users.noreply.github.com>
2016-09-07 23:32:32 -0400

    Working with classes, constructors, and methods.

---
+++ TowerGame/Game.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TowerDefense
+{
+  class Game
+  {
+    public static void Main()
+    {
+      Map map = new Map(8, 5);
+      Point point = new Point(4, 2);
+      
+      Console.WriteLine(point.DistanceTo(5, 5));
+    }
+  }
+}
