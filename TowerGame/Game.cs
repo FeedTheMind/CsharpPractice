9685727fbcded60fbccd89c5a8076a6e89ab6429
FeedTheMind <FeedTheMind@users.noreply.github.com>
2016-09-17 21:56:26 -0400

    Created exceptions and added more comments.

--- TowerGame/Game.cs
+++ TowerGame/Game.cs
@@ -7,9 +7,22 @@ namespace TowerDefense
     public static void Main()
     {
       Map map = new Map(8, 5); // This is an object. Also, an object is an instance of a class (or type).
-      Point point = new Point(4, 2);
-      
-      Console.WriteLine(point.DistanceTo(5, 5));
+      try
+      {
+        MapLocation mapLocation = new MapLocation(20, 20, map);
+      }
+      catch(OutOfBoundsException ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+      catch(TowerDefenseException)
+      {
+        Console.WriteLine("Unhandled TowerDefenseException");
+      }
+      catch(Exception)
+      {
+        Console.WriteLine("Unhandled Exception"); //This accesses the "custom" message.
+      }
     }
   }
 }
--- TowerGame/Point.cs
+++ TowerGame/Point.cs
@@ -13,12 +13,12 @@ namespace TowerDefense
       Y = y;
     }
 
-    public int DistanceTo(int x, int y)
+    public int DistanceTo(int x, int y) // This is a method.
     {
       return (int)Math.Sqrt(Math.Pow(X-x, 2) + Math.Pow(Y-y, 2));
     }
 
-    public int DistanceTo(Point point)
+    public int DistanceTo(Point point) // This is an overloaded method.
     {
       return DistanceTo(point.X, point.Y);
     }
